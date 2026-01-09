93bbc48fa852e383c4b579143e17dd661e96fc53
Mario <gil.gumar25@triana.salesianos.edu>
2026-01-09 14:00:11 +0100

    Saludo y despedida

--- ProyectoJava/src/proyecto/Principal.java
+++ ProyectoJava/src/proyecto/Principal.java
@@ -4,7 +4,10 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("Bienvenido a nuestro programa");
+		
+		System.out.println("Hasta la próxima");
 	}
 
 }
